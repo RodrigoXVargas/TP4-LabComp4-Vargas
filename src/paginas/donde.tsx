ef46370a6c3a9686c83f0d43391ea493a70ba90e
Rodrigo Xavier Vargas <90809287+RodrigoXVargas@users.noreply.github.com>
2025-03-13 09:59:39 -0300

    Update donde.tsx

--- src/paginas/donde.tsx
+++ src/paginas/donde.tsx
@@ -10,7 +10,7 @@ function Donde() {
             <div className='Donde-content'>
                 <div>
                     <h1>Nuestra ubicacion</h1>
-                    <iframe className='Mapa' height={500} width={500} src="https://www.google.com/maps/embed/v1/place?q=Av.%20Las%20Heras%20y%20Av.%20San%20Martin%2C%20Ciudad%20de%20Mendoza&key=AIzaSyBEq7nhkPKV-rRkGcFmEWeAtICCTWf7pxs"></iframe> 
+                    <iframe className='Mapa' height={500} width={500} src="https://www.google.com/maps/embed/v1/place?q=Av.%20Las%20Heras%20y%20Av.%20San%20Martin%2C%20Ciudad%20de%20Mendoza&"></iframe> 
                 </div>
             </div>
         </div>
